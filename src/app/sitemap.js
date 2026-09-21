83e1841b871785eecd55c14b517647113d3e00af
AJAYsiloliya <ajaysiloliya73@gmail.com>
2026-09-21 16:54:01 +0530

    Add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://invitenest-three.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://invitenest-three.vercel.app/templates",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://invitenest-three.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://invitenest-three.vercel.app/pricing",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://invitenest-three.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
